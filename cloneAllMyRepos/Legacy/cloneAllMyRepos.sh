a6c9cd5434b8111218cf1a62152429c5a2e7ba9e
Juan P. Romano <programming@jpromano.net>
2022-09-26 13:24:24 -0700

    Update cloneAllMyRepos.sh

--- cloneAllMyRepos/Legacy/cloneAllMyRepos.sh
+++ cloneAllMyRepos/Legacy/cloneAllMyRepos.sh
@@ -1,17 +1,3 @@
-#!/bin/bash
-# Clone all github.com repos for my user
+Use this shit
 
-echo Insert your github username or organization name:
-read userName
-
-echo
-echo About to start cloning all your repos to this dir...
-echo
-
-for repo in `curl -s https://api.github.com/users/$userName/repos?per_page=1000 |grep git_url |awk '{print $2}'| sed 's/"\(.*\)",/\1/'`;do
-git clone $repo;
-done;
-
-echo
-echo All your repos are cloned, task finished!
-echo
+https://www.npmjs.com/package/clone-all-github-repos
